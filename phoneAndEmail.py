25c620c70138de84a092923f3a8df7e29444fb77
Sarovan <44712196+Sarovan@users.noreply.github.com>
2024-09-17 16:59:07 -0400

    work

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -1,30 +1,40 @@
 import pyperclip, re
 
 phoneRegex = re.compile(
-    r"""
-    (\d{3}|\(\d{3}\))?              # area code
-    (\s|-|.)?                       # separator
+    r"""(
+    (\d{3}|\(\d{3}\))+?              # area code
+    (\s|-|\.)?                       # separator
     (\d{3})                         # first 3 digits
-    (\s|-|.)?                       # separator
+    (\s|-|\.)?                       # separator
     (\d{4})                         # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
-                      """,
+                      )""",
     re.VERBOSE,
 )
 
-# TODO: Create email regex
+# Create email regex
 emailRegex = re.compile(
-    r"""
-    ([a-zA-z0-9_.-]+)               # username
-    (@)                             # separator
-    ([a-zA-z0-9-]+)                 # domain beginning
-    (.com|.edu|.org)$               # domain ending
-                      """,
+    r"""(
+    [a-zA-Z0-9._%+-]+               # username
+    @                               # separator
+    [a-zA-Z0-9-.]+                  # domain
+    \.[a-zA-Z]{2,4}               # top level domain
+                      )""",
     re.VERBOSE,
 )
 
-print(emailRegex.search("asdfasd akazmi001@gmail.com asdfasd"))
 
-# TODO: Find matches in clipboard text
+# Find matches in clipboard text
+sitedata = str(pyperclip.paste())
+matches = []
+for phone in phoneRegex.findall(sitedata):
+    phone_num = "-".join((phone[1], phone[3], phone[5]))
+    matches.append(phone_num)
+for email in emailRegex.findall(sitedata):
+    matches.append(email)
+print("Copied to clipboard:\n")
+for match in matches:
+    print(match)
 
-# TODO: Copy results to clipboard
+# Copy results to clipboard
+pyperclip.copy("\n".join(matches))
